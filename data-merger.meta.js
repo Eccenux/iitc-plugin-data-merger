c14e8fef905d66900ad8ce4e13f6036a0a26a759
Nux <eccenux+git@gmail.com>
2022-02-04 23:03:35 +0100

    0.3.1: intel url update

--- data-merger.meta.js
+++ data-merger.meta.js
@@ -3,11 +3,12 @@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @name           IITC plugin: Data merger (uniques sync)
 // @category       Misc
-// @version        0.3.0
-// @description    [0.3.0] Allows to merge (sync) data across devices and even accounts. For now handles merging uniques (captures and visits).
+// @version        0.3.1
+// @description    [0.3.1] Allows to merge (sync) data across devices and even accounts. For now handles merging uniques (captures and visits).
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
--- data-merger.user.js
+++ data-merger.user.js
@@ -3,14 +3,15 @@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @name           IITC plugin: Data merger (uniques sync)
 // @category       Misc
-// @version        0.3.0
-// @description    [0.3.0] Allows to merge (sync) data across devices and even accounts. For now handles merging uniques (captures and visits).
+// @version        0.3.1
+// @description    [0.3.1] Allows to merge (sync) data across devices and even accounts. For now handles merging uniques (captures and visits).
 // @include        https://intel.ingress.com/*
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://intel.ingress.com/*
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // @updateURL      https://github.com/Eccenux/iitc-plugin-data-merger/raw/master/data-merger.meta.js
 // @downloadURL    https://github.com/Eccenux/iitc-plugin-data-merger/raw/master/data-merger.user.js
